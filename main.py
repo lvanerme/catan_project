ab44233007b749d03b6602f47456c905d2aabbc8
lance_vanermen@taylor.edu <lvanerme@cse21703.cs.local>
2022-12-11 15:43:38 -0500

    new connection established

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# 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
+# Lance's pat for github ^
+
 import psycopg2
 
 conn = psycopg2.connect("dbname=catan_db user=catan_user password=catan_user port=5432 host=roller.cse.taylor.edu")
