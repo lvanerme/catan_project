df376c739863627de8540861c79575d81ce3bcf7
Noah Gegner <noahgegner13@gmail.com>
2022-12-11 17:52:38 -0500

    inserts tiles

--- queries/create_game.py
+++ queries/create_game.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2.extensions import AsIs
 import random
 
 
@@ -19,11 +20,14 @@ def create_game(cur):
     game_id = cur.fetchone()[0]
 
     # insert board
-    cur.execute("INSERT INTO board (game_id) VALUES (%s) RETURNING id", (game_id,))
+    gameID = AsIs('"gameID"')
+    cur.execute(f"INSERT INTO board (game_id) VALUES (%s) RETURNING id", (game_id,))
     board_id = cur.fetchone()[0]
 
     # insert tiles
     tile_values = get_tile_values(board_id)
+    args = ','.join(cur.mogrify("(%s,%s,%s,%s,%s)", i).decode('utf-8') for i in tile_values)
+    cur.execute(f"INSERT INTO tile (board_id, number, location_row, location_col, robber) VALUES {args}")
 
     return cur
 
@@ -33,41 +37,43 @@ def get_tile_values(board_id) -> list:
     tiles = []
 
     for col in range(0, 3):
-        num = random.choice(tile_numbers)
-        tile_numbers.remove(num)
-
-        tile = (board_id, num, 0, col, False)
+        tile = get_tile(board_id, 0, col, tile_numbers)
+        tile_numbers.remove(tile[1])
         tiles.append(tile)
 
     for col in range(0, 4):
-        num = random.choice(tile_numbers)
-        tile_numbers.remove(num)
-
-        tile = (board_id, num, 1, col, False)
+        tile = get_tile(board_id, 1, col, tile_numbers)
+        tile_numbers.remove(tile[1])
         tiles.append(tile)
 
     for col in range(0, 5):
-        num = random.choice(tile_numbers)
-        tile_numbers.remove(num)
-
-        tile = (board_id, num, 2, col, False)
+        tile = get_tile(board_id, 2, col, tile_numbers)
+        tile_numbers.remove(tile[1])       
         tiles.append(tile)
 
     for col in range(0, 4):
-        num = random.choice(tile_numbers)
-        tile_numbers.remove(num)
-
-        tile = (board_id, num, 3, col, False)
+        tile = get_tile(board_id, 3, col, tile_numbers)
+        tile_numbers.remove(tile[1])        
         tiles.append(tile)
 
     for col in range(0, 3):
-        num = random.choice(tile_numbers)
-        tile_numbers.remove(num)
-
-        tile = (board_id, num, 4, col, False)
+        tile = get_tile(board_id, 4, col, tile_numbers)
+        tile_numbers.remove(tile[1]) 
         tiles.append(tile)
 
     return tiles
+
+
+def get_tile(board_id, row, col, tile_numbers: list) -> tuple:
+    num = random.choice(tile_numbers)
+
+    robber = True if num == -1 else False
+    tile = (board_id, num, row, col, robber)
+
+    return tile
+
+
+
 
 main()
 
