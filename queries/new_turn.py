ea507d254526184f83bf4206e1aac544a7f7b892
lvanerme <103009757+lvanerme@users.noreply.github.com>
2022-12-12 19:22:12 -0500

    changes in queries

--- queries/new_turn.py
+++ queries/new_turn.py
@@ -1,8 +1,23 @@
+import psycopg2
 import random
 
+##creates a new turn with a game id and a roll
+def main():
+    conn = psycopg2.connect("dbname=catan_db user=catan_user password=catan_user port=5432 host=roller.cse.taylor.edu")
+    cur = conn.cursor()
 
-def new_turn(cur, game_id, player_id) -> int:
-    roll = random.randint(2, 13)
-    cur.execute(f"INSERT INTO turn (game_id, player_id, roll) VALUES({game_id}, {player_id}, {roll}) RETURNING id")
+    ##needs to pipeline to take in unique input
+    new_turn(cur, 1, 12, 4)
 
-    return cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+
+
+
+def new_turn(cur, g_id, roll, p_id):
+    
+    #insert into turn 
+    cur.execute("""INSERT INTO turn (game_id, roll, player_id) VALUES(%s, %s, %s)""", (g_id, roll, p_id))
+    return cur
+
+main()
